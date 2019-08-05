202d22c068a7c8c7770a4a9b86ea1488b0658c92
Bret Palsson <bretep@gmail.com>
2019-08-05 17:43:06 -0600

    Shred all disks and shutdown when complete

--- custom_config/run.sh
+++ custom_config/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "debian-live" > /etc/hostname
-
+mkdir -p /etc/systemd/system/getty@.service.d
+cp /custom_config/root-auto-login.conf /etc/systemd/system/getty@.service.d/override.conf
+cp /custom_config/bashrc /root/.bashrc
 
 apt-get clean
 
